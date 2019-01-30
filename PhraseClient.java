d10eb79a7231b5a67d4e396c631b38b7e5fff98f
matt2tech <mlipsey@basecampcodingacademy.org>
2019-01-30 12:49:19 -0600

    Added method for client side to interact with server

--- PhraseClient.java
+++ PhraseClient.java
@@ -1,2 +1,26 @@
+import java.io.*;
+import java.net.*;
+
 public class PhraseClient {
+
+    public void go() {
+        try {
+            Socket sock = new Socket("127.0.0.1", 5000);
+
+            InputStreamReader streamReader = new InputStreamReader(sock.getInputStream());
+            BufferedReader reader = new BufferedReader(streamReader);
+
+            String phrase = reader.readLine();
+            System.out.printf("Your lucky phrase: %s", phrase);
+
+            reader.close();
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+        public static void main(String[] args) {
+            PhraseClient client = new PhraseClient();
+            client.go();
+        }
 }
