b0b870b5e02e0c3b9b2ca8a977a1a45200e1af64
matt2tech <mlipsey@basecampcodingacademy.org>
2019-01-30 13:00:46 -0600

    Added method for server side to send phrases to client side

--- PhraseServer.java
+++ PhraseServer.java
@@ -1,2 +1,31 @@
+import java.io.*;
+import java.net.*;
+
 public class PhraseServer {
+    String[] phraseList = {"And another one bites the dust!", "Don't eat yellow snow",
+    "Did I leave the stove on?", "Bazinga!", "What's up?",
+    "This is a public service announcement."};
+
+    public void go() {
+        try {
+            ServerSocket serverSocket = new ServerSocket(5000);
+
+            while(true) {
+                Socket sock = serverSocket.accept();
+
+                printWriter writer = new PrintWriter(sock.getOutputStream());
+                String phrase = getPhrase();
+                writer.println(phrase);
+                writer.close();
+                System.out.println(phrase);
+            }
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        PhraseServer server = new PhraseServer();
+        server.go();
+    }
 }
